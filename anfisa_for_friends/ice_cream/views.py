e77eca4c09eafbf6243fb0b2f3523ff8b9a41707
Ayrat Khasanov <aihasanov87@gmail.com>
2024-12-07 10:05:26 +0300

    Изменение верстки

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -30,4 +30,4 @@ def ice_cream_detail(request, pk):
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
     context = {'ice_cream_list': ice_cream_catalog}
-    return render(request, template, context)
+    return render(request, template, context) 
